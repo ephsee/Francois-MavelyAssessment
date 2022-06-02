d650f395892308a288c2f880bb884432ad2dd441
Francois Spies <fcspies@gmail.com>
2022-06-02 15:01:24 -0400

    trying native

--- App.js
+++ App.js
@@ -1,48 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { AccessibilityInfo ,StyleSheet, Text, View } from 'react-native';
+import { AccessibilityInfo, StyleSheet, Text, View, TextInput, SafeAreaView} from 'react-native';
 
 export default function App() {
 
   console.reportErrorsAsExceptions = false;
 
-  const [apiData, setApiData] = useState('')
+  const [text, setText] = useState('')
+  const [number, setNumber] = useState('') 
 
-  // useEffect(()=>{
-  //   fetch('https://mavely.top/', {mode: 'no-cors'}) // graphql
-  //   .then(r=>r.json())
-  //   .then(setApiData)
-  // },[])
+  // const [apiData, setApiData] = useState('')
 
-  useEffect(() => {
-    // declare the async data fetching function
-    const fetchData = async () => {
-      // get the data from the api
-      const data = await fetch('https://mavely.top/')
-      .then(r=>r.json())
-      .then(setApiData)
-      // convert data to json
-      // const json = await data.json();
-      // return json;
-    }
-  })
+  // useEffect(() => {
+  //   // declare the async data fetching function
+  //   const fetchData = async () => {
+  //     // get the data from the api
+  //     const data = await fetch('https://mavely.top/')
+  //     .then(r=>r.json())
+  //     .then(setApiData)
+  //     console.log(apiData)
+  //     // convert data to json
+  //     // const json = await data.json();
+  //     // return json;
+  //   }
+  // },[]);
 
-    console.log(apiData)
+  const onChangeText = (e) => {
+    setText(e.target.value)
+  }
 
-//   useEffect(() => {
-//     fetch('https://mavely.top/', {mode: 'no-cors'})
-//     .then(r => {
-//       if (r.ok) {
-//           r.json().then(setApiData)
-//       }
-//   })
-// }, [])
+  const onChangeNumber = (e) => {
+    setNumber(e.target.value)
+  }
+
+  console.log(text)
+  console.log(number)
 
   return (
+    <>
     <View style={styles.container}>
       <Text>Maverly Search Assessment</Text>
       <StatusBar style="auto" />
     </View>
+
+    
+    <TextInput
+      style={styles.container}
+      onChangeText={(e)=> onChangeText(e)}
+      placeholder='something text'
+      type='text'
+    />
+    <TextInput
+      style={styles.container}
+      onChangeText={(e)=> onChangeNumber(e)}
+      placeholder="something number"
+      keyboardType="numeric"
+      type='number'
+    />
+    
+    </>
   );
 }
 
