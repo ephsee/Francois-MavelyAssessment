6ae3b42e77111ccb74a42612f9a3fb7f1b49c1a5
Francois Spies <fcspies@gmail.com>
2022-06-02 14:32:54 -0400

    trying native

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { AccessibilityInfo ,StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.reportErrorsAsExceptions = false;
+
+  const [apiData, setApiData] = useState('')
+
+  // useEffect(()=>{
+  //   fetch('https://mavely.top/', {mode: 'no-cors'}) // graphql
+  //   .then(r=>r.json())
+  //   .then(setApiData)
+  // },[])
+
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      // get the data from the api
+      const data = await fetch('https://mavely.top/')
+      .then(r=>r.json())
+      .then(setApiData)
+      // convert data to json
+      // const json = await data.json();
+      // return json;
+    }
+  })
+
+    console.log(apiData)
+
+//   useEffect(() => {
+//     fetch('https://mavely.top/', {mode: 'no-cors'})
+//     .then(r => {
+//       if (r.ok) {
+//           r.json().then(setApiData)
+//       }
+//   })
+// }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Maverly Search Assessment</Text>
       <StatusBar style="auto" />
     </View>
   );
